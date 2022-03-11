e4eff0489960e8f9d8054c873cdb83320494155e
mahabubmonir339 <mahabubhossenmonir@gmail.com>
2022-03-11 15:16:50 +0600

    qustionAnswer

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
+  const userImage = post.userImage
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -62,8 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
-
+                    <img src="${userImage}" alt="User Picture">
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
